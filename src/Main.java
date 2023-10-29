7693a1b85e0dbf58be7db316279edc33cf3f5f5b
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-29 12:21:12 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        boolean bool = false;
+
+        if(!(a == b) || !bool) {
+            System.out.println("UwU");
+        }
+    }
+}
